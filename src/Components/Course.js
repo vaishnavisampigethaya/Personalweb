7ca01e238a6811dfae108612bb5751ca35961907
vaishnavisampigethya <vaishnavisampigethya@gmail.com>
2022-03-01 11:33:40 +0530

    third commit

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -20,85 +20,97 @@ export default function Course(props) {
         <h3>Java</h3>
         <br></br>
         <div className="sub">
-          <a href="https://www.geeksforgeeks.org/java/">
+          <a href="https://www.geeksforgeeks.org/java/" target="_blank">
             <img src={java} alt="Java" height="50%" width="62%" />
             <p>Java Course from geeks for geeks</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.w3schools.com/java/">
+          <a href="https://www.w3schools.com/java/" target="_blank">
             <img src={java1} alt="Java" height="50%" width="62%" />
             <p>Java Course from W3 school</p>
           </a>
         </div>
         <h3>HTML</h3>
         <div className="sub">
-          <a href="https://www.w3schools.com/html/">
+          <a href="https://www.w3schools.com/html/" target="_blank">
             <img src={html} alt="HTML" height="50%" width="62%" />
             <p>HTML Course from W3 school</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.javatpoint.com/html-tutorial">
+          <a href="https://www.javatpoint.com/html-tutorial" target="_blank">
             <img src={html1} alt="HTML" height="50%" width="62%" />
             <p>HTML Course from Javatpoint</p>
           </a>
         </div>
         <h3>CSS</h3>
         <div className="sub">
-          <a href="https://www.w3schools.com/w3css/defaulT.asp">
+          <a href="https://www.w3schools.com/w3css/defaulT.asp" target="_blank">
             <img src={CSS} alt="CSS" height="50%" width="62%" />
             <p>CSS Course from W3 school</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.tutorialspoint.com/css/index.htm">
+          <a
+            href="https://www.tutorialspoint.com/css/index.htm"
+            target="_blank"
+          >
             <img src={css} alt="CSS" height="50%" width="62%" />
             <p>CSS Course from Tutorialspoint</p>
           </a>
         </div>
         <h3>C</h3>
         <div className="sub">
-          <a href="https://www.geeksforgeeks.org/c-programming-language/">
+          <a
+            href="https://www.geeksforgeeks.org/c-programming-language/"
+            target="_blank"
+          >
             <img src={c} alt="C" height="50%" width="62%" />
             <p>C Course from Geeks for Geeks</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.programiz.com/c-programming">
+          <a href="https://www.programiz.com/c-programming" target="_blank">
             <img src={c1} alt="C" height="50%" width="62%" />
             <p>C Course from Programiz</p>
           </a>
         </div>
         <h3>C++</h3>
         <div className="sub">
-          <a href="https://www.geeksforgeeks.org/cpp-tutorial/">
+          <a href="https://www.geeksforgeeks.org/cpp-tutorial/" target="_blank">
             <img src={cp} alt="C++" height="50%" width="62%" />
             <p>C++ Course from Geeks for Geeks</p>
           </a>
         </div>
         <h3>Python</h3>
         <div className="sub">
-          <a href="https://www.w3schools.com/python/">
+          <a href="https://www.w3schools.com/python/" target="_blank">
             <img src={python} alt="python" height="50%" width="62%" />
             <p>Python Course from W3Schools</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.geeksforgeeks.org/python-programming-language/learn-python-tutorial/">
+          <a
+            href="https://www.geeksforgeeks.org/python-programming-language/learn-python-tutorial/"
+            target="_blank"
+          >
             <img src={python1} alt="python" height="50%" width="62%" />
             <p>Python Course from Geeks for Geeks</p>
           </a>
         </div>
         <div className="sub">
-          <a href="https://www.tutorialspoint.com/python/index.htm">
+          <a
+            href="https://www.tutorialspoint.com/python/index.htm"
+            target="_blank"
+          >
             <img src={python2} alt="python" height="50%" width="62%" />
             <p>Python Course from Tutorialspoint</p>
           </a>
         </div>
         <h3>JavaScript</h3>
         <div className="sub">
-          <a href="https://www.w3schools.com/js/">
+          <a href="https://www.w3schools.com/js/" target="_blank">
             <img src={javas} alt="javascript" height="50%" width="62%" />
             <p>JavaScript Course from W3Schools</p>
           </a>
